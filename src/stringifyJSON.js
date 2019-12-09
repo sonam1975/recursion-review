5af113b7552d97c31ff7ebb1729fe5a0ac5de360
sonam1975 <mingmar7@gmail.com>
2019-12-09 15:40:25 -0800

    corrected test for stringify and updated codes for stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //check whats passed in (string? object? function?)
+  //
+  if (typeof(obj) === 'string') {
+
+    return '"' + obj + '"';
+  } if (typeof(obj) === 'number') {
+    return obj.toString();
+  } if (typeof(obj) === 'boolean') {
+    return obj.toString();
+  } if (obj === null) {
+    return 'null';
+  } if (typeof obj === undefined) {
+    return undefined;
+  } if (Array.isArray(obj)) {
+    var result = '';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + result.slice(0, result.length - 1) + ']';
+  } if (Object.prototype.toString.call(obj).split(' ')[1] === 'Object]') {
+    var objResult = '';
+    for (var keys in obj) {
+      if (stringifyJSON(obj[keys])) {
+        objResult += stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]) + ',';
+      }
+    }
+    return '{' + objResult.slice(0, objResult.length - 1) + '}';
+  }
+
+
+
 };
