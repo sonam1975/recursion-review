9db30923df90d98bfd6e200efc3f2d43419fe94c
sonam1975 <mingmar7@gmail.com>
2019-12-09 20:10:34 -0800

    a

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,26 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  //Html
+  //start with an empty array
+  //if we find the clasname then we use recusrsive function over the
+  var result = [];
+  if (this !== window) {
+    var childNodes = this.childNodes;
+  } else {
+    var childNodes = this.document.childNodes;
+  }
+  for (var i = 0; i < childNodes.length; i++) {
+    console.log(childNodes[i])
+    if (childNodes[i].classList && childNodes[i].classList.indexOf(className) ) {
+      result.push(childNodes[i]);
+    } else {
+
+      childNodes[i].getElementsByClassName(className);
+    }
+  }
+
+
+  return result;
+
 };
